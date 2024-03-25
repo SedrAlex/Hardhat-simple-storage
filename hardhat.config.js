8481c1c5b85ced7152262527020dec4126404e0d
SedrAlex313 <124774403+SedrAlex313@users.noreply.github.com>
2024-03-25 04:24:07 -0400

    add hardhat-gas-reporter and solidity coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,10 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomicfoundation/hardhat-ethers");
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 /** @type import('hardhat/config').HardhatUserConfig */
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const privateKey = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COIN_MARKET_CAP = process.env.COIN_MARKET_CAP;
 console.log(process.env.PRIVATE_KEY);
 module.exports = {
   defaultNetwork: "hardhat",
@@ -24,4 +27,12 @@ module.exports = {
   },
   solidity: "0.8.8",
   etherscan: {},
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COIN_MARKET_CAP,
+    token: "MATIC",
+  },
 };
